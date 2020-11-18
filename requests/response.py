91fb37e8b79a906b84bbd726291eaa6c2d5e71d3
pengchenyu <1106998960@qq.com>
2020-11-18 21:08:23 +0800

    requests——response.

---
+++ requests/response.py
@@ -0,0 +1,17 @@
+import requests
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
+}
+
+r = requests.get('http://www.jianshu.com', headers=headers)
+print(type(r.status_code), r.status_code)
+print(type(r.headers), r.headers)
+print(type(r.cookies), r.cookies)
+print(type(r.url), r.url)
+print(type(r.history), r.history)
+
+if not r.status_code == requests.codes.okay:
+    print("failed")
+else:
+    print("ok")
